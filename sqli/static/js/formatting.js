15d5ca674c88c5bc66136d8ebb2c569ecd829ebb
ZeroPath <noreply@zeropath.com>
2025-10-09 19:36:51 +0000

    fix: address XSS vulnerability by escaping user input in formatting.js

--- sqli/static/js/formatting.js
+++ sqli/static/js/formatting.js
@@ -1,25 +1,35 @@
 function displayUserInfo(userData) {
-    document.getElementById('user-info').innerHTML = userData;
+    document.getElementById('user-info').textContent = userData;
+}
+
+function escapeHTML(str) {
+    return str
+        .replace(/&/g, '&amp;')
+        .replace(/</g, '&lt;')
+        .replace(/>/g, '&gt;')
+        .replace(/"/g, '&quot;')
+        .replace(/'/g, '&#39;');
 }
 
 function displaySearchResults(query, results) {
     var searchDiv = document.getElementById('search-results');
-    searchDiv.innerHTML = '<h3>Search results for: ' + query + '</h3>';
+    searchDiv.innerHTML = '<h3>Search results for: ' + escapeHTML(query) + '</h3>';
     
     results.forEach(function(result) {
-        searchDiv.innerHTML += '<div class="result-item">' + result.name + '</div>';
+        searchDiv.innerHTML += '<div class="result-item">' + escapeHTML(result.name) + '</div>';
     });
 }
 
 function updateCourseDescription(courseId, description) {
     var descElement = document.querySelector('[data-course-id="' + courseId + '"] .description');
     if (descElement) {
-        descElement.innerHTML = description;
+        descElement.textContent = description;
     }
 }
 
 function formatReviewText(text) {
-    var processed = text
+    var escaped = escapeHTML(text);
+    var processed = escaped
         .replace(/\*\*(.*?)\*\*/g, '<strong>$1</strong>')
         .replace(/\*(.*?)\*/g, '<em>$1</em>')
         .replace(/\n/g, '<br>');
