ff5ebb5ecfa610e7aaa90ff33ce5f0056566c38a
ZeroPath <noreply@zeropath.com>
2025-10-09 19:37:05 +0000

    fix: mitigate SQL injection by using parameterized queries in create() and search_by_name()

--- sqli/dao/student.py
+++ sqli/dao/student.py
@@ -39,8 +39,8 @@ class Student(NamedTuple):
 
     @staticmethod
     async def create(conn: Connection, name: str):
-        q = ("INSERT INTO students (name) "
-             "VALUES ('%(name)s')" % {'name': name})
+        q = "INSERT INTO students (name) VALUES (%(name)s)"
+        params = {'name': name}
         async with conn.cursor() as cur:
             await cur.execute(q, params)
     
@@ -50,7 +50,7 @@ class Student(NamedTuple):
         q = "SELECT id, name FROM students WHERE name ILIKE %(pattern)s"
         params = {'pattern': f'%{search_term}%'}
         async with conn.cursor() as cur:
-            await cur.execute(q)
+            await cur.execute(q, params)
             results = await cur.fetchall()
             return [Student.from_raw(r) for r in results]
 
